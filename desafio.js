70c1065e4e629585b8015c5ce40f4147db2b077f
stephanievitoria <stehvitoria01@outlook.com>
2023-12-10 13:39:55 -0300

    atualizado

---
+++ desafio.js
@@ -0,0 +1,44 @@
+// O objetivo é criar uma variável com nome do herói e quantidade de XP dele, utilizando uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+// XP < 1.000 = Ferro
+// 1.001 < XP < 2.000 = Bronze
+// 2.001 < XP < 5.000 = Prata
+// 6.001 < XP < 7.000 = Ouro 
+// 7.001 < XP < 8.000 = Platina
+// 8.001 < XP < 9.000 = Ascendente
+// 9.001 < XP < 10.000 = Imortal 
+// XP >= 10.001 = Radiante
+
+//Definindo nome e quantidade
+let nomeDoHeroi = "Athena";
+let quantidadeDeXP = 7.500;
+
+//Exibindo mensagem final
+const mensagem = 'O Herói de nome {nomeDoHeroi} está no nível de {quantidadeDeXP}.';
+
+if (quantidadeDeXP === 1.001 < 2.000){
+    console.log (mensagem);
+}
+else if (quantidadeDeXP === 2.001 < 3.000) {
+    console.log (mensagem);
+}
+else if (quantidadeDeXP === 3.001 < 4.000) {
+    console.log (mensagem);
+}
+else if (quantidadeDeXP === 4.001 < 5.000) {
+    console.log (mensagem);
+}
+else if (quantidadeDeXP === 5.001 < 6.000) {
+    console.log (mensagem);
+}
+else if (quantidadeDeXP === 7.001 < 8.000) {
+    console.log (mensagem);
+}
+else if (quantidadeDeXP === 8.001 < 9.000) {
+    console.log (mensagem);
+}
+else if (quantidadeDeXP === 9.001 < 10.000) {
+    console.log (mensagem);
+}
+else {
+    console.log (mensagem);
+}
